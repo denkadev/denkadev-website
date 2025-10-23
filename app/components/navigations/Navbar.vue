87df21cac079de1c87ecb4da18ce0353b126b13c
Denka <denkadev@gmail.com>
2025-10-23 21:39:54 +0700

    feat: add dummy navbar

---
+++ app/components/navigations/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-menu
+    :default-active="activeIndex"
+    mode="horizontal"
+    :ellipsis="false"
+    @select="handleSelect"
+  >
+    <el-menu-item index="0">
+        DENKA
+    </el-menu-item>
+    <el-menu-item index="1">MENU 1</el-menu-item>
+    <el-menu-item index="2">MENU 2</el-menu-item>
+    <el-menu-item index="3">MENU 3</el-menu-item>
+    <el-menu-item index="4">
+        <Icon name="octicon:mark-github-16" width="32" height="32" />
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const activeIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
+
+<style scoped>
+.el-menu--horizontal > .el-menu-item:nth-child(1) {
+  margin-right: auto;
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,10 @@
 <template>
     <div>
-        Index Page
+        <Navbar></Navbar>
+        Dummy Content
     </div>
 </template>
+
+<script lang="ts" setup>
+    import Navbar from "@/components/navigations/Navbar.vue"
+</script>
